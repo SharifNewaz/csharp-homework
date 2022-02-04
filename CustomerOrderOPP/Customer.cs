6ace9d1f7a0b1fbd57228c9634a15462903dd94f
Mohammad <sharifnewaz1996@gmail.com>
2022-02-03 21:38:55 -0800

    add new project

---
+++ CustomerOrderOPP/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CustomerOrderOOP
+{
+	public class Customer
+	{
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Name{get{ return FirstName + LastName;}}
+        public string? Email { get; set; }
+        public Order? Orders { get; set; }
+    }
+
+	public class Order
+    {
+        //Earlier GUID was known as UUID.
+        //UUID stands for Universally Unique Identifier.
+        //We use GUID for unique identifier.
+        public Order(Guid ordNum)
+        {
+            OrderNumber = ordNum;
+        }
+        public Guid OrderNumber { get; }
+    }
+}
+
---
+++ CustomerOrderOPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomerOrderOPP{
+
+    class Program
+    {
+        public static void Main()
+        {
+            System.Guid guid = System.Guid.NewGuid();
+            Console.WriteLine(guid.ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
