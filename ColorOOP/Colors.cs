cc66c4da85b26304fcb6cd34664d467f1cfcefda
Mohammad <sharifnewaz1996@gmail.com>
2022-02-03 19:56:55 -0800

    add changes

---
+++ ColorOOP/Colors.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ColorOOP
+{
+	public class Colors
+	{
+        public Colors(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public Colors(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+
+        public int grayscaleValue()
+        {
+            return (Red + Green + Blue) / 3;
+        }
+    }
+
+	public class Ball : Colors
+    {
+        public Ball(int ballSize, byte red, byte green, byte blue, byte alpha):base(red, green,blue, alpha)
+        {
+            BallSize = ballSize;
+            TimesBallThrown = 0;
+        }
+
+        public int BallSize;
+        public int TimesBallThrown { get; set; }
+
+        public void Pop()
+        {
+            BallSize = 0;
+        }
+
+        public void ThrowBall()
+        {
+            if(BallSize > 0)
+            {
+                TimesBallThrown++;
+            }
+        }
+    }
+}
+
---
+++ ColorOOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ColorOOP
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //instantiate and print the values
+            Ball firstBall = new (5, 12, 32, 12, 5);
+            Console.WriteLine("First Ball: ");
+            Console.WriteLine("Size = " + firstBall.BallSize);
+            Console.WriteLine("Red = " + firstBall.Red);
+            Console.WriteLine("Green = "+ firstBall.Green);
+            Console.WriteLine("Blue = "+ firstBall.Blue);
+            Console.WriteLine("First Ball thrown " + firstBall.TimesBallThrown + " times");
+            Console.WriteLine();
+
+            Ball secondBall = new(6, 124, 232, 57, 55);
+            Console.WriteLine("Second Ball: ");
+            Console.WriteLine("Size = " + secondBall.BallSize);
+            Console.WriteLine("Red = " + secondBall.Red);
+            Console.WriteLine("Green = " + secondBall.Green);
+            Console.WriteLine("Blue = " + secondBall.Blue);
+            Console.WriteLine("Second Ball thrown " + secondBall.TimesBallThrown + " times");
+            Console.WriteLine();
+
+            Ball thirdBall = new(98, 12, 32, 200, 43);
+            Console.WriteLine("Third Ball: ");
+            Console.WriteLine("Size = " + thirdBall.BallSize);
+            Console.WriteLine("Red = " + thirdBall.Red);
+            Console.WriteLine("Green = " + thirdBall.Green);
+            Console.WriteLine("Blue = " + thirdBall.Blue);
+            Console.WriteLine("Third Ball thrown " + thirdBall.TimesBallThrown + " times");
+            Console.WriteLine();
+
+            Console.WriteLine("After throwing the ball around we should see different values for Ballthrown.");
+
+            //throw the ball around
+            firstBall.ThrowBall();
+            secondBall.ThrowBall();
+            firstBall.ThrowBall();
+            thirdBall.ThrowBall();
+            firstBall.ThrowBall();
+            thirdBall.ThrowBall();
+            Console.WriteLine();
+
+            Console.WriteLine("First Ball thrown " + firstBall.TimesBallThrown + " times");
+            Console.WriteLine("Second Ball thrown " + secondBall.TimesBallThrown + " times");
+            Console.WriteLine("Third Ball thrown " + thirdBall.TimesBallThrown + " times");
+
+            Console.ReadLine();
+        }
+    }
+}
